834c0fa080b65e82b1e0198b79bb00b65dadd749
Cory House <cory.house@carrier.com>
2021-08-17 15:22:36 -0400

    delete food api

--- e-gineering/src/api/foodsApi.ts
+++ e-gineering/src/api/foodsApi.ts
@@ -1,3 +1,11 @@
 export async function getFoods() {
   return fetch("http://localhost:3001/foods");
 }
+
+export async function deleteFood(id: number) {
+  const response = await fetch("http://localhost:3001/foods/" + id, {
+    method: "DELETE",
+  });
+  if (!response.ok) throw new Error("Delete failed");
+  return await response.json();
+}
