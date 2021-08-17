883bab98eaff5252d1f07123d4fe977c6746d05c
Cory House <cory.house@carrier.com>
2021-08-17 13:07:14 -0400

    Display foods

--- e-gineering/src/App.tsx
+++ e-gineering/src/App.tsx
@@ -1,3 +1,28 @@
+// Ale emporium - Hermanaki Sauce.
+// BottleWorks
+// Ma ma Korean BBQ
+
+import React from "react";
+
+type Food = {
+  name: string;
+  quantity: number;
+};
+
+const foods: Food[] = [
+  { name: "Carrot", quantity: 1 },
+  { name: "Potato", quantity: 2 },
+];
+
 export function App() {
-  return <h1>Pantry Manager</h1>;
+  function renderFoods() {
+    return foods.map((food) => <li>{food.name}</li>);
+  }
+
+  return (
+    <>
+      <h1>Pantry Manager</h1>
+      <ul>{renderFoods()}</ul>
+    </>
+  );
 }
