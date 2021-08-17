e85a690d39e732bd7ffe8c5e96e82e2a4c317581
Cory House <cory.house@carrier.com>
2021-08-17 13:35:10 -0400

    Implement table

--- e-gineering/src/App.tsx
+++ e-gineering/src/App.tsx
@@ -7,22 +7,37 @@ import React from "react";
 type Food = {
   name: string;
   quantity: number;
+  minQuantity: number;
+  type: string;
 };
 
 const foods: Food[] = [
-  { name: "Carrot", quantity: 1 },
-  { name: "Potato", quantity: 2 },
+  { name: "Carrot", quantity: 1, minQuantity: 5, type: "Veggie" },
+  { name: "Potato", quantity: 2, minQuantity: 3, type: "Veggie" },
 ];
 
 export function App() {
-  function renderFoods() {
-    return foods.map((food) => <li>{food.name}</li>);
-  }
-
   return (
     <>
       <h1>Pantry Manager</h1>
-      <ul>{renderFoods()}</ul>
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Min Quantity</th>
+          <th>Type</th>
+        </thead>
+        <tbody>
+          {foods.map((food) => (
+            <tr>
+              <td>{food.name}</td>
+              <td>{food.quantity}</td>
+              <td>{food.minQuantity}</td>
+              <td>{food.type}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
