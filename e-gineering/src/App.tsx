4d06ecbaf42dfb484ae539a31f65a38ce8a25290
Cory House <cory.house@carrier.com>
2021-08-17 15:39:07 -0400

    Implement delete

--- e-gineering/src/App.tsx
+++ e-gineering/src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import { getFoods } from "./api/foodsApi";
+import { getFoods, deleteFood } from "./api/foodsApi";
 
 type Food = {
+  id: number;
   name: string;
   quantity: number;
   minQuantity: number;
@@ -14,7 +15,6 @@ export function App() {
   useEffect(() => {
     async function callGetFoods() {
       const response = await getFoods();
-      debugger;
       if (!response.ok) throw new Error("Call to get foods failed");
       const json: Food[] = await response.json();
       setFoods(json);
@@ -29,6 +29,7 @@ export function App() {
       <table>
         <thead>
           <tr>
+            <th></th>
             <th>Name</th>
             <th>Quantity</th>
             <th>Min Quantity</th>
@@ -36,9 +37,20 @@ export function App() {
           </tr>
         </thead>
         <tbody>
-          x
           {foods.map((food) => (
             <tr key={food.name}>
+              <td>
+                <button
+                  onClick={async () => {
+                    await deleteFood(food.id);
+                    // Return a new array with the id that was just deleted omitted.
+                    const newFoods = foods.filter((f) => f.id !== food.id);
+                    setFoods(newFoods);
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
               <td>{food.name}</td>
               <td>{food.quantity}</td>
               <td>{food.minQuantity}</td>
