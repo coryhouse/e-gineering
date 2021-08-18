17226b926c8ff49f219a0327c85bf38c57d7af89
Cory House <cory.house@carrier.com>
2021-08-18 14:22:42 -0400

    Add onChange and label

--- e-gineering/src/shared/Select.tsx
+++ e-gineering/src/shared/Select.tsx
@@ -9,22 +9,30 @@ type SelectProps = {
   options: SelectOption[];
   placeholderOption: string;
   value: string;
+  onChange?: React.ChangeEventHandler<HTMLSelectElement>;
 };
 
 // Destructuring props within the method signature to avoid repeating the word props.
-export function Select({ placeholderOption, value, options }: SelectProps) {
+export function Select({
+  id,
+  placeholderOption,
+  value,
+  options,
+  onChange,
+  label,
+}: SelectProps) {
   return (
-    <select>
-      <option value="">{placeholderOption}</option>
-      {options.map((option) => (
-        <option
-          selected={value === option.value}
-          key={option.value}
-          value={option.value}
-        >
-          {option.label}
-        </option>
-      ))}
-    </select>
+    <div>
+      <label htmlFor={id}>{label}</label>
+      <br />
+      <select id={id} onChange={onChange} value={value}>
+        <option value="">{placeholderOption}</option>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 }
