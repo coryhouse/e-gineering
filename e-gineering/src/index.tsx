723fa70b8375384029e776fd8ad4b0ca3babeb7e
Cory House <cory.house@carrier.com>
2021-08-18 15:41:46 -0400

    add react router

--- e-gineering/src/index.tsx
+++ e-gineering/src/index.tsx
@@ -1,5 +1,8 @@
 import { render } from "react-dom";
 import { App } from "./App";
+import { BrowserRouter, Route } from "react-router-dom";
+import { About } from "./About";
+import { Nav } from "./Nav";
 
 // HTML                         vs  JSX
 // class                            className
@@ -7,4 +10,15 @@ import { App } from "./App";
 // inline styles are strings        Inline styles are objects, Numbers = px.
 // <!-- comments like this -->      {/* comments like this */}
 // attributes are kebab-cased       props are camelCased
-render(<App />, document.getElementById("root"));
+render(
+  <BrowserRouter>
+    <Nav />
+    <Route path="/about">
+      <About />
+    </Route>
+    <Route path="/" exact>
+      <App />
+    </Route>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
