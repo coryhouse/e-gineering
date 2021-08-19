d76289bc93669df94455f25f8bd8708a5d484066
Cory House <cory.house@carrier.com>
2021-08-19 10:59:15 -0400

    Consume env and add getFood

--- e-gineering/src/api/foodsApi.ts
+++ e-gineering/src/api/foodsApi.ts
@@ -1,14 +1,22 @@
 import { Food } from "../App";
 import { NewFood } from "../FoodForm";
 
+const baseUrl = process.env.REACT_APP_BASE_URL;
+
 export async function getFoods() {
-  const response = await fetch("http://localhost:3001/foods");
+  const response = await fetch(baseUrl + "foods");
   if (!response.ok) throw new Error("Call to get foods failed");
   return response.json() as Promise<Food[]>;
 }
 
+export async function getFood(id: number) {
+  const response = await fetch(baseUrl + "foods/" + id);
+  if (!response.ok) throw new Error("Call to get food failed");
+  return response.json() as Promise<Food>;
+}
+
 export async function deleteFood(id: number) {
-  const response = await fetch("http://localhost:3001/foods/" + id, {
+  const response = await fetch(baseUrl + "foods/" + id, {
     method: "DELETE",
   });
   if (!response.ok) throw new Error("Delete failed");
@@ -16,7 +24,7 @@ export async function deleteFood(id: number) {
 }
 
 export async function addFood(food: NewFood) {
-  const response = await fetch("http://localhost:3001/foods", {
+  const response = await fetch(baseUrl + "foods", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
